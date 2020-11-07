14e12f3443af535f09e26c109bf897251e319e70
docmistersir <wsb6157@rit.edu>
2020-11-07 11:44:21 -0500

    FMOD integration
    integrated FMOD and updated gitignore

---
+++ Assets/Audio/AudioTesting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTesting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/TestSounds/Glitch_1");
+    }
+}
