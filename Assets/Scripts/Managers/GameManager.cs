bf0fcaa446921b94a0350c32c60813f32fcd2f6a
AJ Chapman <ajc7345@rit.edu>
2020-11-14 12:03:16 -0500

    some work on InputData

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InputData.Initialize();
     }
 
     // Update is called once per frame
@@ -29,8 +29,6 @@ public class GameManager : MonoBehaviour
             {
                 allNodes.Add(nodes[i].GetComponent<Node>());
             }
-
-
         }
     }
 }
