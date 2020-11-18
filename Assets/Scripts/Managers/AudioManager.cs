4b5d91c05820b618e594324524e5295e9efa1818
AJ Chapman <ajc7345@rit.edu>
2020-11-18 13:22:17 -0500

    audioManager

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    //Fields
+    public static AudioManager instance = null;
+
+    //Methods
+    //Turns AudioManagers into serial killers who survive scene transistions and murder any other baby GameManagers they encounter
+    void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
